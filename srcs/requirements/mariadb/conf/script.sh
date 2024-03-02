aa87dd8546fae68120e48a5624f3e09eb9b4fb44
Your Name <you@example.com>
2024-03-02 19:47:59 +0100

    wordpress/mariadb is working

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-# mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
 chown -R mysql:mysql /var/lib/mysql
+
+sed -i "s#bind-address            = 127.0.0.1#bind-address=*#g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 
 service mariadb start
 
 sleep 10
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mariadb -hlocalhost -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -hlocalhost -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -hlocalhost -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -hlocalhost -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+mariadb -hlocalhost -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
 
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -2,24 +2,25 @@
 
 sleep 10
 
-until mariadb -h "mariadb" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e ";" ; do
-            echo "Waiting until connect to MariaDB database..."
-        sleep 1
-done
-
-echo "Connected succefully to MariaDB database..."
-
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-    wget https://wordpress.org/wordpress-6.4.3.tar.gz -P /var/www
-    cd /var/www && tar -xzf wordpress-6.4.3.tar.gz  && rm wordpress-6.4.3.tar.gz 
-    wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --path='/var/www/wordpress'
-    wp core install --allow-root --path='/var/www/wordpress' --url=$DOMAIN --title=inception --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$ADMIN_EMAIL
-    wp user create --allow-root --path='/var/www/wordpress' $USER $USER@gmail.com --user_pass=$USER
-fi
+wget https://wordpress.org/wordpress-6.4.3.tar.gz -P /var/www
+cd /var/www && tar -xzf wordpress-6.4.3.tar.gz  && rm wordpress-6.4.3.tar.gz
 
 chmod -R 755 /var/www/wordpress
-mkdir /run/php
+
+cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/wordpress/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/wordpress/wp-config.php
+sed -i "s/localhost/$MYSQL_HOST/g" /var/www/wordpress/wp-config.php
+
+
+wp core install --allow-root --path='/var/www/wordpress' --url=$DOMAIN --title=inception --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$ADMIN_EMAIL
+wp user create --allow-root --path='/var/www/wordpress' $USER $USER@gmail.com --user_pass=$USER
+
+
+if [ ! -d /run/php ]; then
+    mkdir /run/php 
+fi
 
 exec php-fpm7.4 -F
-
-
